90b0d567b70116abcf7d57f30973d75f773c0752
kapakrystian <56194438+kapakrystian@users.noreply.github.com>
2024-09-10 15:17:11 +0200

    Jobs searching by title or tag.

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+
+class SearchController extends Controller
+{
+    public function __invoke()
+    {
+        /**
+         * In job table search all of jobs which have got a 'q' value in their title.
+         */
+        $jobs = Job::where('title', 'LIKE', '%' . request('q') . '%')->get();
+
+        return view('results', ['jobs' => $jobs]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,24 @@
 
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SearchController;
 use App\Http\Controllers\SessionController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, 'index']);
+Route::get('/search', SearchController::class);
+
+/**
+ * The default value of wild card is id (/tags/{tag}) but
+ * we can indicate another value which will be binding parameter (/tags/{tag:name}).
+ */
+Route::get('/tags/{tag:name}', TagController::class);
 
 Route::middleware('guest')->group(function () {
     Route::get('/register', [RegisteredUserController::class, 'create']);
     Route::post('/register', [RegisteredUserController::class, 'store']);
-    
+
     Route::get('/login', [SessionController::class, 'create']);
     Route::post('/login', [SessionController::class, 'store']);
 });
